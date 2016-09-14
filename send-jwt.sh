60fc61a4e64810d4c9eea20b4437e7fbb7894219
zekaf <jose.faisca@gmail.com>
2016-09-14 00:32:53 +0000

    Update

--- send-jwt.sh
+++ send-jwt.sh
@@ -4,8 +4,8 @@
 # Send JWT on the HTTP Authorization header 
 #
 
-JWT="xxxxxxxxxxxxx3"
-HOST="http://localhost"
+JWT=$(cat access_token)
+HOST="http://192.168.15.54"
 PORT=8888
 
 curl -H "Authorization: Token ${JWT}" ${HOST}:${PORT} || printf '%s\n' $?
