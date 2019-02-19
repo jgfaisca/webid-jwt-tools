07afc6a5e496bb7a4bcb025d36a870b4df7c59e4
zekaf <jose.faisca@gmail.com>
2019-02-19 12:26:29 +0000

    Update create-jwt.sh

--- create-jwt.sh
+++ create-jwt.sh
@@ -10,6 +10,7 @@ FILE1="header_payload"
 FILE2="unencoded_token"
 FILE3="access_token"
 WALLET_PW="secret"
+UNLOCK_SEC=10
 
 function error(){
   code=$1
@@ -29,32 +30,30 @@ fi
 # check file
 [ ! -f $FILE1 ] && error 1
 
+# create message
 header=$(awk 'NR==1' $FILE1)
 payload=$(awk 'NR==2' $FILE1)
-
-# get message
 message=$header.$payload
 
-# unlock wallet for 10 seconds
-namecoin-cli -datadir=$DATA_DIR walletpassphrase "${WALLET_PW}" 10 &&
+# unlock wallet for n seconds
+namecoin-cli -datadir=$DATA_DIR walletpassphrase "${WALLET_PW}" $UNLOCK_SEC &>/dev/null
 
-# sign
-signature=$(namecoin-cli -datadir=$DATA_DIR signmessage "${NMC_ADDRESS}" "${message}") &&
+# sign message
+signature=$(namecoin-cli -datadir=$DATA_DIR signmessage "${NMC_ADDRESS}" "${message}") 
 
+# crate unencoded_token
 unencoded_token="$message.$signature"
 echo $unencoded_token > $FILE2
 
-# Stripping the JWT parts Header.Payload.Signature into an array
-declare -a ujwt
-
-[ -f $FILE3 ] && rm -f $FILE3
-touch $FILE3
-
-# Encode
+# create access_token
 enc="$(echo -n "$header" | base64 | tr -d '\n')"
 enc="$enc.$(echo "$payload" | base64 | tr -d '\n')"
 enc="$enc.$(echo "$signature" | base64 | tr -d '\n')"
 
+# save acces_token to file
+[ -f $FILE3 ] && rm -f $FILE3
+touch $FILE3
 echo $enc > $FILE3
 
+# print access_token
 cat $FILE3
