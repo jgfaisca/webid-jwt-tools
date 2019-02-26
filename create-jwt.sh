1180f9f0638837e93942a3731f3323f28c488487
zekaf <jose.faisca@gmail.com>
2019-02-26 13:42:06 +0000

    Update create-jwt.sh

--- create-jwt.sh
+++ create-jwt.sh
@@ -7,9 +7,7 @@
 
 # error function
 function error(){
-  code=$1
-  [ $code -eq 1 ] && echo "Error: File not found."
-  [ $code -eq 2 ] && echo "Error: Wrong number of JWT elements ($elements)"
+  echo "Error: File $1 not found."
   exit 1
 }
 
@@ -25,25 +23,23 @@ function replaceVar(){
 # variables
 TMP_DIR="tmp/jwt
 CONF_DIR="conf"
-. $CONF_DIR/jwt.conf
+JWT_CONF_FILE="$CONF_DIR/jwt/jwt.conf"
+DLT_CONF_FILE="$CONF_DIR/dlt/wallet.conf"
+HEADER_TEMPLATE=
+PAYLOAD_TEMPLATE=
 iss="$ISSUER"
-NMC_ADDRESS=""
-#DATADIR="$HOME/.namecoin"
-DATA_DIR="/data/namecoin"
-WALLET_PW="secret"
-UNLOCK_SEC=10
 
 # create temporary directory
-mkdir -p $TMP_DIR
+[ -d "$TMP_DIR" ] || mkdir -p $TMP_DIR
 
 # get address value from NMC
 nshow=$(namecoin-cli -datadir=$DATA_DIR name_show "$iss")
 NMC_ADDRESS=$(echo $nshow | python -c "import sys, json; print json.load(sys.stdin)['address']")
 
 # create message
-[ -r "$CONF_DIR/header.template" ] && cp $CONF_DIR/header.template $TMP_DIR/header || error 1
-[ -r "$CONF_DIR/payload.template" ] && cp $CONF_DIR/payload.template $TMP_DIR/payload || error 1
-[ -r "$CONF_DIR/jwt.con" ] || error 1
+[ -r "$CONF_DIR/jwt/header.template" ] && cp $CONF_DIR/jwt/header.template $TMP_DIR/header || error "$CONF_DIR/jwt/header.template"
+[ -r "$CONF_DIR/jwt/payload.template" ] && cp $CONF_DIR/jwt/payload.template $TMP_DIR/payload || error "$CONF_DIR/jwt/payload.template"
+[ -r "$CONF_DIR/jwt/jwt.con" ] || error "$CONF_DIR/jwt/jwt.con
 replaceVar "ALGORITHM" ${ALGORITHM} $TMP_DIR/header
 replaceVar "ISSUER" "${ISSUER}" $TMP_DIR/payload
 if [ -z "$EXPIRYDATE" ]; then
@@ -58,10 +54,10 @@ message=$header.$payload
 echo $message > $TMP_DIR/message
 
 # unlock wallet for n seconds
-namecoin-cli -datadir=$DATA_DIR walletpassphrase "${WALLET_PW}" $UNLOCK_SEC &>/dev/null
+namecoin-cli -datadir=$NMC_DATA_DIR walletpassphrase "${NMC_WALLET_PWD}" $NMC_UNLOCK_SEC &>/dev/null
 
 # sign message
-signature=$(namecoin-cli -datadir=$DATA_DIR signmessage "${NMC_ADDRESS}" "${message}") 
+signature=$(namecoin-cli -datadir=$NMC_DATA_DIR signmessage "${NMC_ADDRESS}" "${message}") 
 
 # create unencoded_token
 unencoded_token="$message.$signature"
