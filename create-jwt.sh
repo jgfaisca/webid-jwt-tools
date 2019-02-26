25f5093559a3380653ff4aa88e6e06618de17e88
zekaf <jose.faisca@gmail.com>
2019-02-26 13:14:23 +0000

    Update create-jwt.sh

--- create-jwt.sh
+++ create-jwt.sh
@@ -23,37 +23,39 @@ function replaceVar(){
 }
 
 # variables
-. conf/jwt.conf
+TMP_DIR="tmp/jwt
+CONF_DIR="conf"
+. $CONF_DIR/jwt.conf
 iss="$ISSUER"
 NMC_ADDRESS=""
 #DATADIR="$HOME/.namecoin"
 DATA_DIR="/data/namecoin"
-FILE0="conf/header.template"
-FILE1="conf/payload.template"
-FILE2="unencoded_token"
-FILE3="access_token"
 WALLET_PW="secret"
 UNLOCK_SEC=10
+
+# create temporary directory
+mkdir -p $TMP_DIR
 
 # get address value from NMC
 nshow=$(namecoin-cli -datadir=$DATA_DIR name_show "$iss")
 NMC_ADDRESS=$(echo $nshow | python -c "import sys, json; print json.load(sys.stdin)['address']")
 
 # create message
-[ -r "$FILE0" ] && cp $FILE0 header || error 1
-[ -r "$FILE1" ] && cp $FILE1 payload || error 1
-replaceVar "ALGORITHM" ${ALGORITHM} header
-replaceVar "ISSUER" "${ISSUER}" payload
+[ -r "$CONF_DIR/header.template" ] && cp $CONF_DIR/header.template $TMP_DIR/header || error 1
+[ -r "$CONF_DIR/payload.template" ] && cp $CONF_DIR/payload.template $TMP_DIR/payload || error 1
+[ -r "$CONF_DIR/jwt.con" ] || error 1
+replaceVar "ALGORITHM" ${ALGORITHM} $TMP_DIR/header
+replaceVar "ISSUER" "${ISSUER}" $TMP_DIR/payload
 if [ -z "$EXPIRYDATE" ]; then
     DATE=$(perl -e '$x=time+(${HOURS}*3600);print $x')
-    replaceVar "EXPIRYDATE" "${DATE}" payload
+    replaceVar "EXPIRYDATE" "${DATE}" $TMP_DIR/payload
 else
-    replaceVar "EXPIRYDATE" "${EXPIRYDATE}" payload
+    replaceVar "EXPIRYDATE" "${EXPIRYDATE}" $TMP_DIR/payload
 fi
-header=$(cat header)
-payload=$(cat payload)
+header=$(cat $TMP_DIR/header)
+payload=$(cat $TMP_DIR/payload)
 message=$header.$payload 
-echo $message > message
+echo $message > $TMP_DIR/message
 
 # unlock wallet for n seconds
 namecoin-cli -datadir=$DATA_DIR walletpassphrase "${WALLET_PW}" $UNLOCK_SEC &>/dev/null
@@ -63,15 +65,15 @@ signature=$(namecoin-cli -datadir=$DATA_DIR signmessage "${NMC_ADDRESS}" "${mess
 
 # create unencoded_token
 unencoded_token="$message.$signature"
-echo $unencoded_token > $FILE2
+echo $unencoded_token > $TMP_DIR/unencoded_token
 
 # create access_token 
 enc="$(echo -n "$header" | base64 | tr -d '\n')"
 enc="$enc.$(echo "$payload" | base64 | tr -d '\n')"
 enc="$enc.$(echo "$signature" | base64 | tr -d '\n')"
-echo $enc > $FILE3
+echo $enc > $TMP_DIR/access_token
 
 # print access_token
-cat $FILE3
+cat $TMP_DIR/access_token
 
 exit 0
