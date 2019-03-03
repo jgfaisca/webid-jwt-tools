8922f0e3dd0fc76bb5389aba6c64cc48377e5502
zekaf <jose.faisca@gmail.com>
2019-03-03 23:53:21 +0000

    Update sparql-wallet.py

--- sparql-wallet.py
+++ sparql-wallet.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python
 # dependencies:
-# pip install rdfextras rdflib rdflib-sparql
+# pip install rdfextras rdflib
 
 import sys
 import rdflib
 from rdflib import Graph
 
-profile_doc = sys.argv[1]
+profile_doc = sys.argv[1] 
+expStr = sys.argv[2] 
 
 g = Graph()
 g.parse(profile_doc)
 
-query = """
-SELECT ?namecoin WHERE {
-   ?wallet cc:namecoin ?namecoin .
-}
-"""
-for row in g.query(query):
-   print row
+# OnlineAccount accountName(s) of the document author
+qres = g.query(
+   """SELECT DISTINCT ?oan
+      WHERE {
+        ?doc rdf:type foaf:PersonalProfileDocument ;
+        foaf:maker ?author .
+        ?author foaf:account ?account .
+        ?account rdf:type foaf:OnlineAccount .
+        ?account foaf:accountName ?oan .
+        FILTER regex(str(?oan), '"""+expStr+"""', "i")
+      }""")
+
+for row in qres:
+    network,address = str(row[0]).split(':')
+    print (address)
+
+g.close()
