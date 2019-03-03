6a53550c34223dc5169380a21319194dcd66c5fa
zekaf <jose.faisca@gmail.com>
2019-03-03 23:46:32 +0000

    Update sparql-triples-wallet.py

--- sparql-triples-wallet.py
+++ sparql-triples-wallet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
 # dependencies:
-# pip install rdfextras rdflib 
+# pip install rdfextras rdflib
 #
 # Authors:
 # Jose G. Faisca <jose.faisca@gmail.com>
@@ -9,32 +9,19 @@
 
 import sys
 import rdflib
-from rdflib import Graph 
-from rdflib import URIRef 
-# import time
+from rdflib import Graph
+from rdflib import URIRef
+from rdflib.namespace import FOAF
 
-profile_doc = sys.argv[1]
+profile_doc = sys.argv[1] 
+expStr = sys.argv[2] 
 
 g = Graph()
-# start = time.time()
 g.parse(profile_doc)
-# mid = time.time()
 
-# mid2 = time.time()
+for subj, pred, obj in g.triples((None,FOAF.accountName,None)):
+    if obj.startswith(expStr):
+       network,address = obj.split(":")
+       print(address)
 
-#print("--- printing raw triples ---")
-#for subj, pred, obj in g:
-#    print((subj, pred, obj))
-#
-#print("----------------------------")
-
-for subj, pred, obj in g.triples((None,URIRef("https://w3id.org/cc#wallet"),None)):
-        network,address = obj.split(":")
-        print (address)
-
-# end = time.time()
-
-# print "file"
-# print mid-start
-# print "query"
-# print end-mid2
+g.close()
