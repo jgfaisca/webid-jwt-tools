adbfdc36103a9f7fa430e3936bedc61a739af13d
zekaf <jose.faisca@gmail.com>
2019-03-03 23:50:01 +0000

    Update sparql-person.py

--- sparql-person.py
+++ sparql-person.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python
 # dependencies:
-# pip install rdfextras rdflib rdflib-sparql
+# pip install rdfextras rdflib
 
 import sys
 import rdflib
 from rdflib import Graph
+from rdflib.plugins.sparql import prepareQuery
+from rdflib.namespace import RDF, FOAF
+from rdflib import URIRef
 
 profile_doc = sys.argv[1]
 
 g = Graph()
 g.parse(profile_doc)
 
-query = """
-SELECT ?name WHERE {
-       ?Person foaf:name ?name .
-}
-"""
-for row in g.query(query):
-   print row
+qres = prepareQuery(
+   """SELECT DISTINCT ?name
+      WHERE {
+        ?doc rdf:type foaf:PersonalProfileDocument ;
+        foaf:maker ?author .
+        ?author foaf:name ?name .
+      }""", initNs = {'foaf':FOAF, 'rdf':RDF})
+
+for row in g.query(qres):
+        print("%s" % row)
+
+g.close()
