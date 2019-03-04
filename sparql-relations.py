596bc9964afc52e2cffe66f5cc9c126b0ed30adc
Ubuntu <ubuntu@cloud1-lxc0.inetlab.lan>
2019-03-04 16:25:08 +0000

    Update

---
+++ sparql-relations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#
+# Description
+# Document maker social relations
+#
+# Dependencies:
+# pip install rdfextras rdflib
+#
+
+import sys
+import rdflib
+from rdflib import Graph
+from rdflib.plugins.sparql import prepareQuery
+from rdflib.namespace import FOAF
+
+profile_doc = sys.argv[1]
+
+g = Graph()
+g.parse(profile_doc)
+
+qres = prepareQuery(
+    """SELECT DISTINCT ?aname ?bname
+       WHERE {
+          ?a foaf:knows ?b .
+          ?a foaf:name ?aname .
+          ?b foaf:name ?bname .
+       }""", initNs = {'foaf':FOAF})
+
+for row in g.query(qres):
+    print("%s knows %s" % row)
+
+g.close()
