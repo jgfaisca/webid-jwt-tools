461d9545209ead8c9269829152d07f633cf3498c
zekaf <jose.faisca@gmail.com>
2019-03-12 22:12:14 +0000

    Update auth-jwt.sh

--- auth-jwt.sh
+++ auth-jwt.sh
@@ -5,19 +5,18 @@
 #
 
 # chech arguments
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
   echo
   echo "Invalid number of arguments."
-  echo "Usage: ./$(basename "$0") <host> <port>"
+  echo "Usage: ./$(basename "$0") <uri>"
   echo
   exit 1
 fi
 
 # variables
-HOST=$1
-PORT=$2
+URI=$1
 
 PRODUCE="./produce-jwt.sh"
 SEND="./send-jwt.sh"
 
-eval '"$PRODUCE"' && eval '"$SEND"' $HOST $PORT || exit 1
+eval '"$PRODUCE"' && eval '"$SEND"' $URI || exit 1
