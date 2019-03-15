7c820c9f852a1db5eaf0fd55cb93ddd9bdc101cc
zekaf <jose.faisca@gmail.com>
2019-03-15 20:38:16 +0000

    Update https-forwarding.sh

--- https-forwarding.sh
+++ https-forwarding.sh
@@ -20,15 +20,13 @@ CONSUMER_CONF="$CONF_DIR/jwt/consumer/consumer.conf"
 [ -s "$CONSUMER_CONF" ] || error "$CONSUMER_CONF"
 . $CONSUMER_CONF
 
+# log file
 [ -f "$LOG_HTTPS" ] && rm -f "$LOG_HTTPS"
 touch $LOG_HTTPS
 
 # verbosity level (can be used several times)
-[ "$VERBOSE" == "true" ] && VARG="-v" || V_ARG=""
+[ "$VERBOSE" == "true" ] && V_ARG="-v" || V_ARG=""
 
 # HTTPS -> HTTP
 #In this case an SSL/TLS connection to $ADDR_SSL on port $PORT_SSL is piped to the remote host $ADDR on port $PORT.
 socat $V_ARG -lf $LOG_HTTPS openssl-listen:$PORT_SSL,bind=$ADDR_SSL,cert=$HOST_CRT,key=$HOST_KEY,verify=0,reuseaddr,fork tcp4:$ADDR:$PORT
-
-
-
